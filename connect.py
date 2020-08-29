27f4d9035ffc956d4db3dbc7baf8739cdf000507
Kingsley Obeng <mystakingz@gmail.com>
2020-08-29 09:48:31 +0200

    Add files via upload

--- connect.py
+++ connect.py
@@ -40,21 +40,23 @@ def winning_move(board, piece):
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT-3):
             if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
-                return True
+                return True   
             
-     #positively slope diagonal 
-     for c in range(COLUMN_COUNT-3):
-         for r in range(ROW_COUNT-3):
-             if board[r][c] == piece and board[r+1][c-1] == piece and board[r+2][c-2] == piece and board[r+3][c-3] == piece:
-                 return True  
-
-     #negatively slope diagonal 
-     for c in range(COLUMN_COUNT-3):
+    #negatively slope diagonal 
+    for c in range(COLUMN_COUNT-3):
          for r in range(3, ROW_COUNT):
              if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
-                 return True        
+                 return True   
+             
+     # positively slope diagonal
+    for c in range(COLUMN_COUNT-3):
+         for r in range(ROW_COUNT-3):
+             if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                 return True
 
-board= create_board()
+     
+
+board = create_board()
 print_board(board)
 
 turn=0
@@ -74,7 +76,7 @@ while not game_over:
         col = int(input("Player 2 type a number(0-6)"))
         if is_valid_location(board, col):
             row= get_next_open_row(board, col)
-            drop_piece(board,col,row,2)
+            drop_piece(board, col, row, 2)
             if winning_move(board, 2):
                 print("Player 2 WINS!! ")
                 game_over=True
